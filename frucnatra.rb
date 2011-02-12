f5b065cd182644b0adafc9c695f1cdf6b819286a
Richard Wang <a2h@live.com.au>
2011-02-12 20:20:10 +1100

    Return instead of setting a flag when a route is found

--- frucnatra.rb
+++ frucnatra.rb
@@ -37,42 +37,38 @@ require :phpcall
     path_info = $server[:PATH_INFO] || '/'
     method = $server[:REQUEST_METHOD]
     
-    route_found = false # temp
-    
     if routes = $routes[method]
       routes.each do |arr| # Splatting to block params not supported yet
         pattern, keys, conditions, block = arr
 
         if pattern == path_info
           puts block.call
-          route_found = true
+          return
         end
       end
     end
     
-    if !route_found
-      # This is meant to be (<<-HTML).gsub(/^ {8}/, ''), but Fructose doesn't support regex yet
-      puts (<<-HTML)
-        <!DOCTYPE html>
-        <html>
-        <head>
-          <style type="text/css">
-          body { text-align:center;font-family:helvetica,arial;font-size:22px;
-            color:#888;margin:20px}
-          #c {margin:0 auto;width:500px;text-align:left}
-          </style>
-        </head>
-        <body>
-          <h2>Frucnatra doesn't know this ditty.</h2>
-          <img src='/__sinatra__/404.png'>
-          <div id="c">
-            Try this:
-            <pre>#{method.downcase.escape} '#{path_info.escape}' do\n  "Hello World"\nend</pre>
-          </div>
-        </body>
-        </html>
-      HTML
-    end
+    # This is meant to be (<<-HTML).gsub(/^ {6}/, ''), but Fructose doesn't support regex yet
+    puts (<<-HTML)
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style type="text/css">
+        body { text-align:center;font-family:helvetica,arial;font-size:22px;
+          color:#888;margin:20px}
+        #c {margin:0 auto;width:500px;text-align:left}
+        </style>
+      </head>
+      <body>
+        <h2>Frucnatra doesn't know this ditty.</h2>
+        <img src='/__sinatra__/404.png'>
+        <div id="c">
+          Try this:
+          <pre>#{method.downcase.escape} '#{path_info.escape}' do\n  "Hello World"\nend</pre>
+        </div>
+      </body>
+      </html>
+    HTML
   end
 
   phpcall :register_shutdown_function, 'F_frucnatra_shutdown', nil
