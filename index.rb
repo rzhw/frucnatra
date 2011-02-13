47bb1ba6c020a7829ba0f64a864fc39561ddee74
Richard Wang <a2h@live.com.au>
2011-02-13 15:06:54 +1100

    Added a demo for POST

--- index.rb
+++ index.rb
@@ -2,16 +2,29 @@ require 'http'
 require 'frucnatra'
 
 get '/' do
-  "<h1>Test</h1>
-  <p>This is the homepage!</p>
+  "<h1>Fructose Demo</h1>
+  <p>
+    <form method='post' action='#{$root}/name/submit'>
+      What's your name?
+      <input type='text' name='name'>
+      <input type='submit'>
+    </form>
+  </p>
   <ul>
     <li><a href='#{$root}/test'>Test page</a></li>
     <li><a href='#{$root}/lskdjfklsjdf'>Non-existent page</a></li>
   </ul>"
 end
 
+post '/name/submit' do
+  # No .escape will cause an error
+  "<h1>Fructose Demo</h1>
+  <p>You said your name was #{$post[:name].escape}</p>
+  <p><a href='#{$root}'>Go home</a></p>"
+end
+
 get '/test' do
-  "<h1>Test</h1>
+  "<h1>Fructose Demo</h1>
   <p>This is a test page!</p>
   <p><a href='#{$root}'>Go home</a></p>"
 end
