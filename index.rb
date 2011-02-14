42d03b2824ec8850ce200fb8fd8264f9c7ddfc8a
Richard Wang <a2h@live.com.au>
2011-02-14 19:04:09 +1100

    Updated the demo

--- index.rb
+++ index.rb
@@ -1,36 +1,60 @@
 require 'frucnatra'
 
+@home = "<p><a href='#{root}'>Go home</a></p>"
+
 get '/' do
-  "<h1>Fructose Demo</h1>
-  <p>
-    <form method='post' action='#{root}/name/submit'>
-      What's your name?
-      <input type='text' name='name'>
-      <input type='submit'>
-    </form>
-  </p>
+  "<h1>Fructose Demo</h1>  
+  <h2>Basic</h2>
   <ul>
     <li><a href='#{root}/test'>Test page</a></li>
     <li><a href='#{root}/lskdjfklsjdf'>Non-existent page</a></li>
-  </ul>"
-end
-
-post '/name/submit' do
-  "<h1>Fructose Demo</h1>
-  <p>You said your name was #{params[:name]}</p>
-  <p><a href='#{root}'>Go home</a></p>"
+  </ul>
+  
+  <h2>POST</h2>
+  <p>
+    <form method='post' action='#{root}/name/submit'>
+      Your name:
+      <input type='text' name='name'>
+      <input type='submit' value='Submit'>
+    </form>
+  </p>
+  
+  <h2>Route params</h2>
+  <p>
+    Your name:
+    <input type='text' id='name-p'>
+    <input type='button' value='Submit' onclick='location.href=\"#{root}/name/param/\" + document.getElementById(\"name-p\").value'>
+    <noscript>(need js enabled)</noscript>
+  </p>
+  
+  <h2>Multiple route params</h2>
+  <p>
+    First name:
+    <input type='text' id='name-f'>
+    Last name:
+    <input type='text' id='name-l'>
+    <input type='button' value='Submit' onclick='location.href=\"#{root}/name/param/\" +
+      document.getElementById(\"name-f\").value + \"/\" + document.getElementById(\"name-l\").value'>
+    <noscript>(need js enabled)</noscript>
+  </p>"
 end
 
 get '/test' do
   "<h1>Fructose Demo</h1>
-  <p>This is a test page!</p>
-  <p><a href='#{root}'>Go home</a></p>"
+  <p>This is a test page!</p>" + @home
 end
 
-get '/name/:name' do
-  "<p>You said your name was #{params[:name]}"
+post '/name/submit' do
+  "<h1>Fructose Demo</h1>
+  <p>You said your name was #{params[:name]}</p>" + @home
 end
 
-get '/name/:first/:last' do
-  "<p>You said your name was #{params[:first]} #{params[:last]}"
+get '/name/param/:name' do
+  "<h1>Fructose Demo</h1>
+  <p>You said your name was #{params[:name]}" + @home
+end
+
+get '/name/param/:first/:last' do
+  "<h1>Fructose Demo</h1>
+  <p>You said your name was #{params[:first]} #{params[:last]}" + @home
 end
